987cf66d0a845af41c1cccf2162723b8eb2b706f
Emmanuel Tom Jose <84658038+leantos@users.noreply.github.com>
2024-11-30 11:35:12 +0530

    included props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { Entry } from "./components/Entry";
 import { Header } from "./components/Header";
+import japanImg from '/src/assets/japan.jpeg';
 
 
 function App(){
   return(
     <>
       <Header/>
-      <Entry/>
-      <Entry/>
+      <Entry
+        img = {{
+          src: japanImg,
+          alt:'Fuji'
+        }}
+        country= "JAPAN"
+        location = 'https://maps.app.goo.gl/1137892RPu6jhofy8'
+        title='Mount Fuji'
+        date = '12 Jan, 2021 - 24 Jan,2021'
+        about = 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.'
+      />
     </>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,20 +1,21 @@
-var url = 'https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?cs=srgb&dl=pexels-tomas-malik-793526-3408353.jpg&fm=jpg';
-var location = 'https://maps.app.goo.gl/1137892RPu6jhofy8';
-
-export function Entry(){
+/* eslint-disable react/prop-types */
+export function Entry(props){
     return(
         <>
         <article className="card">
-            <img src={url} className="place-image"/>
+            <img src={props.img.src} 
+            className="place-image"
+            alt={props.img.alt}
+            />
             <div className="place-details">
-                <img className="location-pin" src="/src/assets/location.svg"/>
-                <span>JAPAN</span>
-                <a href={location} target="_blank">View on Google Maps</a>
+                <img className="location-pin" 
+                src="/src/assets/location.svg"/>
+                <span>{props.country}</span>
+                <a href={props.location} target="_blank">View on Google Maps</a>
                 <div className="entry">
-                <h1 className="entry-title">Mount Fuji</h1>
-                <p className="date">12 Jan, 2021 - 24 Jan,2021</p>
-
-                <p className="entry-text">Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.</p>
+                <h1 className="entry-title">{props.title}</h1>
+                <p className="date">{props.date}</p>
+                <p className="entry-text">{props.about}</p>
                 </div>
             </div>
         </article>
