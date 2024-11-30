a7175e98021569c214c593d631e04d52fa722456
Emmanuel Tom Jose <84658038+leantos@users.noreply.github.com>
2024-11-30 12:13:53 +0530

    Added custom data via Maps and Data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
+/* eslint-disable react/jsx-key */
 import { Entry } from "./components/Entry";
 import { Header } from "./components/Header";
-import japanImg from '/src/assets/japan.jpeg';
+import data from './data.js'
+
+const entryElements = data.map((entry)=>{
+  return(
+    <Entry
+      img={entry.img}
+      title={entry.title}
+      location={entry.googleMapsLink}
+      country={entry.country}
+      date={entry.dates}
+      about={entry.text}
+    />
+  )
+})
 
 
 function App(){
   return(
     <>
       <Header/>
-      <Entry
-        img = {{
-          src: japanImg,
-          alt:'Fuji'
-        }}
-        country= "JAPAN"
-        location = 'https://maps.app.goo.gl/1137892RPu6jhofy8'
-        title='Mount Fuji'
-        date = '12 Jan, 2021 - 24 Jan,2021'
-        about = 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.'
-      />
+      <main className="contianer">
+        {entryElements}
+      </main>
     </>
   );
 }
